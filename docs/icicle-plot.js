6f82331e9d030b7e6d4da74f64dc8c603ea3fd2f
Laurent Brock <laurent.brock@epfl.ch>
2023-05-12 15:56:59 +0200

    Trying to resolve async shenanigans, not managed to fix anything yet, but not broken anything either!

--- docs/icicle-plot.js
+++ docs/icicle-plot.js
@@ -1,27 +1,52 @@
 // Read CSV dataset from filesystem and store it in a variable
 
-var dataset = Object();
-var indexing = Object();
+async function readTextFile(fileName) {
+    let dataset = Object();
+    let indexing = Object();
 
-d3.csv("medecines.csv", function(data) {
-    //Convert the CSV into an array of objects
-    if (Number(data[""]) < 7 && Number(data[""]) >= 0) {
-        //??
-    } else {
-        if (data["Content type:"] === "Category") {
-            indexing = Object.keys(data).map(key => data[key]).splice(1);
-            for (let i = 0; i < indexing.length; i++) {
-                dataset[indexing[i]] = new Array();
+
+    return new Promise(function(resolve, reject) {
+    d3.csv(fileName, function(data) {
+
+        if (data) {
+            //Convert the CSV into an array of objects
+            if (Number(data[""]) < 7 && Number(data[""]) >= 0) {
+                //??
+            } else {
+                if (data["Content type:"] === "Category") {
+                    indexing = Object.keys(data).map(key => data[key]).splice(1);
+                    for (let i = 0; i < indexing.length; i++) {
+                        dataset[indexing[i]] = new Array();
+                    }
+                } else {
+                    delete data[""];
+
+                    for (let i = 0; i < indexing.length; i++) {
+                        const dat = Object.values(data);
+                        dataset[indexing[i]].push(dat[i]);
+                    }
+                }
             }
         } else {
-            delete data[""];
-
-            for (let i = 0; i < indexing.length; i++) {
-                const dat = Object.values(data);
-                dataset[indexing[i]].push(dat[i]);
-            }
+            reject(new Error("Failed to read file"));
         }
-    }
-});
+    });
+    resolve(dataset);
+    
+    });
+}
 
-console.log(dataset);
+// Wait for readTextFile to finish reading the file
+async function readData() {
+    let dataset = await readTextFile("medecines.csv");
+    console.log(dataset);
+    return dataset;
+}
+
+// Create the icicle plot
+async function createIciclePlot() {
+    let dataset = await readData();
+    console.log("Placeholder icicle plot function");
+}
+
+createIciclePlot();
