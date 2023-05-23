cd622476bf24d234961e5e285eac93c252f6b53c
Cindy Tang <cindy.tang@epfl.ch>
2023-05-23 11:13:02 +0200

    first layer of icicle plot

--- docs/icicle-plot.js
+++ docs/icicle-plot.js
@@ -1,5 +1,4 @@
 // Read CSV dataset from filesystem and store it in a variable
-
 async function readCSVFile(fileName) {
     return new Promise((resolve, reject) => {
         let dataset = Object();
@@ -118,6 +117,46 @@ async function readData() {
     console.log("Other count: " + other_count);
 
     console.log("Top 10 oncology sub-categories", findTopKSubstrings(getRegexMatchesArray(dataset["Therapeutic area"], oncology_regex), 10));
+
+
+    dataset = {
+        name: "Therapeutic area",
+        value: dataset["Therapeutic area"].length,
+        children: [
+          {
+            name: "oncology",
+            value: oncology_count,
+          },
+          {
+            name: "neuroscience",
+            value: neuroscience_count,
+          },
+          {
+            name: "covid",
+            value: covid_count,
+          },
+          {
+            name: "respiratory",
+            value: respiratory_count,
+          },
+          {
+            name: "cardiovascular",
+            value: cardiovascular_count,
+          },
+          {
+            name: "diabetes",
+            value: diabetes_count,
+          },
+          {
+            name: "cardiovascular",
+            value: metabolic_count,
+          },
+          {
+            name: "other",
+            value: other_count,
+          },
+        ],
+      };
     
     return dataset;
 }
@@ -159,8 +198,107 @@ function findTopKSubstrings(array, k) {
 
 // Create the icicle plot
 async function createIciclePlot() {
-    let dataset = await readData();
-    console.log("Placeholder icicle plot function");
+  // Get our formatted dataset
+  let dataset = await readData();
+
+  // Define the dimensions of the SVG container
+  const width = 1000;
+  const height = 500;
+
+  // Color scale 
+  const color = d3.scaleOrdinal(d3.quantize(d3.interpolateRainbow, dataset.children.length + 1))
+
+  // Number formatter
+  const format = d3.format(",d");
+
+  // Create the hierarchical structure from data
+  const partition = data => {
+    const root = d3.hierarchy(data)
+      .sum(d => d.value)
+      .sort((a, b) => b.value - a.value);
+    return d3.partition()
+      .size([height, (root.height + 1) * width / 3])
+      (root);
+  };
+
+  // Create the icicle plot (https://observablehq.com/@d3/zoomable-icicle)
+  function chart () {
+    const root = partition(dataset);
+    let focus = root;
+  
+    const svg = d3.create("svg")
+        .attr("viewBox", [0, 0, width, height])
+        .style("font", "10px sans-serif");
+  
+    const cell = svg
+      .selectAll("g")
+      .data(root.descendants())
+      .join("g")
+        .attr("transform", d => `translate(${d.y0},${d.x0})`);
+  
+    const rect = cell.append("rect")
+        .attr("width", d => d.y1 - d.y0 - 1)
+        .attr("height", d => rectHeight(d))
+        .attr("fill-opacity", 0.6)
+        .attr("fill", d => {
+          if (!d.depth) return "#ccc";
+          while (d.depth > 1) d = d.parent;
+          return color(d.data.name);
+        })
+        .style("cursor", "pointer")
+        .on("click", clicked);
+  
+    const text = cell.append("text")
+        .style("user-select", "none")
+        .attr("pointer-events", "none")
+        .attr("x", 4)
+        .attr("y", 13)
+        .attr("fill-opacity", d => +labelVisible(d));
+  
+    text.append("tspan")
+        .text(d => d.data.name);
+  
+    const tspan = text.append("tspan")
+        .attr("fill-opacity", d => labelVisible(d) * 0.7)
+        .text(d => ` ${format(d.value)}`);
+  
+    cell.append("title")
+        .text(d => `${d.ancestors().map(d => d.data.name).reverse().join("/")}\n${format(d.value)}`);
+  
+    function clicked(event, p) {
+      focus = focus === p ? p = p.parent : p;
+  
+      root.each(d => d.target = {
+        x0: (d.x0 - p.x0) / (p.x1 - p.x0) * height,
+        x1: (d.x1 - p.x0) / (p.x1 - p.x0) * height,
+        y0: d.y0 - p.y0,
+        y1: d.y1 - p.y0
+      });
+  
+      const t = cell.transition().duration(750)
+          .attr("transform", d => `translate(${d.target.y0},${d.target.x0})`);
+  
+      rect.transition(t).attr("height", d => rectHeight(d.target));
+      text.transition(t).attr("fill-opacity", d => +labelVisible(d.target));
+      tspan.transition(t).attr("fill-opacity", d => labelVisible(d.target) * 0.7);
+    }
+    
+    function rectHeight(d) {
+      return d.x1 - d.x0 - Math.min(1, (d.x1 - d.x0) / 2);
+    }
+  
+    function labelVisible(d) {
+      return d.y1 <= width && d.y0 >= 0 && d.x1 - d.x0 > 16;
+    }
+    
+    return svg.node();
+  }
+
+  // Call the chart function on our dataset
+  const svgElement = chart(dataset);
+
+  // Append the SVG element to the DOM
+  document.getElementById("icicle-plot").appendChild(svgElement);
 }
 
 createIciclePlot();
