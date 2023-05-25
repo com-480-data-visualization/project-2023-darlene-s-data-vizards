af3e4df3a7b864e48f7441e02fe523747f374f99
Laurent Brock <laurent.brock@epfl.ch>
2023-05-25 15:24:01 +0200

    Update the graph

--- docs/icicle-plot.js
+++ docs/icicle-plot.js
@@ -273,77 +273,89 @@ async function createIciclePlot() {
 	};
 
 	// Create the icicle plot (https://observablehq.com/@d3/zoomable-icicle)
-	function chart () {
+	function chart() {
 		const root = partition(dataset);
 		let focus = root;
-	
+	  
 		const svg = d3.create("svg")
-				.attr("viewBox", [0, 0, width, height])
-				// .style("font", "10px sans-serif");
-	
+		  .attr("viewBox", [0, 0, width, height]);
+	  
 		const cell = svg
-			.selectAll("g")
-			.data(root.descendants())
-			.join("g")
-				.attr("transform", d => `translate(${d.y0},${d.x0})`);
-	
+		  .selectAll("g")
+		  .data(root.descendants())
+		  .join("g")
+		  .attr("transform", d => `translate(${d.y0},${d.x0})`);
+	  
 		const rect = cell.append("rect")
-				.attr("width", d => d.y1 - d.y0 - 1)
-				.attr("height", d => rectHeight(d))
-				.attr("fill-opacity", 0.6)
-				.attr("fill", d => {
-					if (!d.depth) return "#ccc";
-					while (d.depth > 1) d = d.parent;
-					return color(d.data.name);
-				})
-				.style("cursor", "pointer")
-				.on("click", clicked);
-	
+		  .attr("width", d => d.y1 - d.y0 - 1)
+		  .attr("height", d => rectHeight(d))
+		  .attr("fill-opacity", 0.6)
+		  .attr("fill", d => {
+			if (!d.depth) return "#ccc";
+			while (d.depth > 1) d = d.parent;
+			return color(d.data.name);
+		  })
+		  .style("cursor", "pointer")
+		  .on("click", clicked);
+	  
 		const text = cell.append("text")
-				.style("user-select", "none")
-				.attr("pointer-events", "none")
-				.attr("x", 4)
-				.attr("y", 16)
-				.attr("fill-opacity", d => +labelVisible(d));
-	
+		  .style("user-select", "none")
+		  .attr("pointer-events", "none")
+		  .attr("x", 4)
+		  .attr("y", 16)
+		  .attr("fill-opacity", d => +labelVisible(d));
+	  
 		text.append("tspan")
-				.text(d => d.data.name);
-	
+		  .text(d => d.data.name);
+	  
 		const tspan = text.append("tspan")
-				.attr("fill-opacity", d => labelVisible(d) * 0.7)
-				.text(d => ` ${format(d.value)}`);
-	
+		  .attr("fill-opacity", d => labelVisible(d) * 0.7)
+		  .text(d => ` ${format(d.value)}`);
+	  
 		cell.append("title")
-				.text(d => `${d.ancestors().map(d => d.data.name).reverse().join("/")}\n${format(d.value)}`);
-	
+		  .text(d => `${d.ancestors().map(d => d.data.name).reverse().join("/")}\n${format(d.value)}`);
+	  
+		// Hide all levels except the top-level
+		rect.attr("fill-opacity", d => d.depth === 0 ? 0.6 : 0);
+		text.attr("fill-opacity", d => d.depth === 0 ? +labelVisible(d) : 0);
+		tspan.attr("fill-opacity", d => d.depth === 0 ? labelVisible(d) * 0.7 : 0);
+	  
 		function clicked(event, p) {
-			focus = focus === p ? p = p.parent : p;
-	
-			root.each(d => d.target = {
-				x0: (d.x0 - p.x0) / (p.x1 - p.x0) * height,
-				x1: (d.x1 - p.x0) / (p.x1 - p.x0) * height,
-				y0: d.y0 - p.y0,
-				y1: d.y1 - p.y0
-			});
-	
-			const t = cell.transition().duration(750)
-					.attr("transform", d => `translate(${d.target.y0},${d.target.x0})`);
-	
-			rect.transition(t).attr("height", d => rectHeight(d.target));
-			text.transition(t).attr("fill-opacity", d => +labelVisible(d.target));
-			tspan.transition(t).attr("fill-opacity", d => labelVisible(d.target) * 0.7);
+		  focus = focus === p ? p = p.parent : p;
+	  
+		  root.each(function (d) {
+			d.target = {
+			  x0: (d.x0 - p.x0) / (p.x1 - p.x0) * height,
+			  x1: (d.x1 - p.x0) / (p.x1 - p.x0) * height,
+			  y0: d.y0 - p.y0,
+			  y1: d.y1 - p.y0
+			};
+		  });
+	  
+		  const t = cell.transition().duration(750)
+			.attr("transform", d => `translate(${d.target.y0},${d.target.x0})`);
+	  
+		  rect.transition(t)
+			.attr("height", d => rectHeight(d.target))
+			.attr("fill-opacity", d => (d.depth === focus.depth || d.parent === focus) ? 0.6 : 0);
+	  
+		  text.transition(t)
+			.attr("fill-opacity", d => (d.depth === focus.depth || d.parent === focus) ? +labelVisible(d.target) : 0);
+	  
+		  tspan.transition(t)
+			.attr("fill-opacity", d => (d.depth === focus.depth || d.parent === focus) ? labelVisible(d.target) * 0.7 : 0);
 		}
-		
+	  
 		function rectHeight(d) {
-			return d.x1 - d.x0 - Math.min(1, (d.x1 - d.x0) / 2);
+		  return d.x1 - d.x0 - Math.min(1, (d.x1 - d.x0) / 2);
 		}
-	
+	  
 		function labelVisible(d) {
-			return d.y1 <= width && d.y0 >= 0 && d.x1 - d.x0 > 16;
+		  return d.y1 <= width && d.y0 >= 0 && d.x1 - d.x0 > 16;
 		}
-		
+	  
 		return svg.node();
-	}
+	  }
 
 	// Call the chart function on our dataset
 	const svgElement = chart(dataset);
